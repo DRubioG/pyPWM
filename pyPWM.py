7bc83135faab5cb088ce1266dcdc6c41c9aad870
David R <davidrubio97@outlook.com>
2024-02-12 19:07:12 +0100

    update

--- pyPWM.py
+++ pyPWM.py
@@ -27,6 +27,32 @@ for i in p[:-1]:
             output.append(0)
         print(j, i, output[-1])
 
+
+def PWM(signal_input, amplitud = 2,period = 10, N_samples = 1000):
+    porcentaje = y1/amplitud
+    # print(porcentaje)
+
+    t=np.linspace(0, period, N_samples)
+    # print(t)
+    porcen = porcentaje*100
+    p = [int(x) for x in porcen]
+    output = []
+
+    for i in p[:-1]:
+        for j in range(100):
+            
+            if (100-j)<=i:
+                output.append(amplitud)
+            else:
+                output.append(0)
+            print(j, i, output[-1])
+
+    return output
+
+
+
+
+
 print(output)
 plt.plot(t, output)
 plt.plot(x, y)
